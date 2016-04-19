33082d349d0ec808ff29fb677f42d46df135fd48
altius <vinodrey916vino@gmail.com>
2016-04-19 11:27:11 +0530

    image test

--- config/boot.rb
+++ config/boot.rb
@@ -4,8 +4,8 @@ require 'bundler/setup' # Set up gems listed in the Gemfile.
 #require 'rails/commands/server'
 #module Rails
  # class Server
-  #  def default_options
-   #   super.merge(Host: "192.168.1.51" ,Port: 3000)
+   # def default_options
+    #  super.merge(Host: "192.168.1.51" ,Port: 3000)
    # end
  # end
 #end
